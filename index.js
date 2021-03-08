6f592ade8a214d08019854b1456175024f37cb13
waramsey <waramsey@unomaha.edu>
2021-03-08 15:05:29 -0700

    Edits for Evacuation Simulation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import "./crowded.js"
 
 import assets from "@crowdedjs/assets"
+import fs from "fs"
 
 let objValue = assets.objs.hospital;       //Grab the value of the environment 
 let locationValue = assets.locations.locationsHospital;  //Grab the value of all the locations
@@ -67,6 +68,10 @@ class App extends CrowdSimApp {
   currentMillisecond = 0;
   millisecondsBetweenFrames = 40; //40ms between frames, or 25fps
   currentTick = 0;
+  arrivals = [];
+  locations = [];
+  activeAgents = [];
+
 
   constructor(objFileContents, secondsOfSimulation, locationFileContents) {
     super();
@@ -74,9 +79,11 @@ class App extends CrowdSimApp {
     this.secondsOfSimulation = secondsOfSimulation;
     this.locations = locationFileContents;
   }
+
   boot(nonce) {
     this.bootMesh(this.objFileContents);
   }
+
   getAgentDefinitions() {
     return { type: "agentDefinitions", agents: JSON.stringify(CrowdSimApp.agents) };
   }
@@ -87,10 +94,11 @@ class App extends CrowdSimApp {
     let i = this.currentTick++;
     if (i < 1) {
       // initialize all agent's id
-
+      
     }
     for (let agent of newAgents) {
       CrowdSimApp.agents.push(agent);
+      this.activeAgents.push(agent)
       let start = this.getStart(agent);
       let idx = this.crowd.addAgent(start, this.getAgentParams(CrowdSimApp.updateFlags));
       agent.idx = idx;
@@ -105,6 +113,7 @@ class App extends CrowdSimApp {
     }
     for (let agent of leavingAgents) {
       agent.inSimulation = false;
+      this.activeAgents.splice(this.activeAgents.indexOf(agent),1);
       CrowdSimApp.agents.find(a => a.idx == agent.idx).inSimulation = false;
       this.crowd.removeAgent(agent.idx);
     }
@@ -130,13 +139,19 @@ class App extends CrowdSimApp {
         toPost.push(toAdd);
       }
     }
-    doneWithFrame({ agents: JSON.stringify(toPost), frame: i })
+    doneWithFrame({ agents: toPost, frame: i }, self)
   }
+
   getStart(agent) {
-    return [agent.startX, agent.startY, agent.startZ]
+    //find coordinates of arrivalLocation
+    let loc = this.locations.find(l => l.name == agent.arrivalLocation)
+    return [loc.position.x, loc.position.y, loc.position.z]
   }
+
   getEnd(agent) {
-    return [agent.destX, agent.destY, agent.destZ]
+    //all agents go to evacuate for now
+    let loc = this.locations.find(l => l.annotationName == "Hospital Entrance")
+    return [loc.position.x, loc.position.y, loc.position.z]
   }
 }
 
@@ -145,13 +160,43 @@ let app;
 async function boot() {
   app = new App(objValue, 10000, locationValue);
   app.boot();
-  await app.tick([{ destX: 0, destY: 0, destZ: 0, id: 0, startX: 60, startY: 0.25, startZ: 6 }], [], []);
+
+  for (const property in arrivalValue) {
+    app.arrivals.push(arrivalValue[property])
+  }
+  for (const property in locationValue) {
+    app.locations.push(locationValue[property])
+  }
+  await app.tick([], [], []);
 }
 
-async function doneWithFrame(options) {
-  console.log(options)
-  if (options.frame < 50)
-    await app.tick([], [], [])
+async function doneWithFrame(options, sim) {
+  let end = sim.getEnd();
+  let remove = [];
+
+  options.agents.forEach(agent => {
+    let pos = options.agents.indexOf(agent)
+    if (options.agents[pos].x > end[0] - 1 && options.agents[pos].x < end[0] + 1)
+      if (options.agents[pos].y > end[1] - 1 && options.agents[pos].y < end[1] + 1)
+        if (options.agents[pos].z > end[2] - 1 && options.agents[pos].z < end[2] + 1) {
+          console.log(`agent ${agent.id} exiting`)
+          remove.push(agent)
+        }
+  });
+
+  if (sim.arrivals.length == 0 && sim.activeAgents.length == 0) {
+    console.log("Done with tick callback.")
+  } else {
+    let temp = [];
+    sim.arrivals.forEach(newAgent => {
+      if (newAgent.arrivalTick <= options.frame) {
+        temp.push(newAgent)
+      }
+    })
+    sim.arrivals = sim.arrivals.slice(temp.length);
+
+    await app.tick(temp, [], remove)
+  }
 }
 
-boot();
+export {boot};
